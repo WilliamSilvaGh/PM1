628da9aa051ae460c316a231276685194f3bb1fd
William <williamsilvaesquilino@gmail.com>
2023-08-04 20:48:52 -0300

    branch com o código da aula do dia Fri, Aug  4, 2023  8:48:52 PM

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Center(
+        
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text("Hello World!"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const[
+                Text("William"),
+                Text("2023"),
+              ],
+            )
+          ]
+
+          
+        ),
+      ),
+    );
+  }
+}
+
+
+class Aplicacao extends StatelessWidget {
+  const Aplicacao({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "App Hello World",
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+void main() => runApp(const Aplicacao());
